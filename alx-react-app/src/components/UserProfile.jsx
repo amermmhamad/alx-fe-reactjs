b8fdccc5b551553c0529449f0b6e447a470622c3
Amer <amermmhamad@gmail.com>
2024-09-12 16:38:21 +0300

    Added UserProfile

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+let UserProfile = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>: {props.age}</p>
+      <p>: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
