65df3238a95613388f509f40812b884d9ea72524
Amer <amermmhamad@gmail.com>
2024-09-21 04:18:08 +0300

    asa

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,17 +1,14 @@
-let UserProfile = (props) => {
-  return (
-    <>
-      <div
-        style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}
-      >
-        <h2 style={{ color: "blue" }}>{props.name}</h2>
-        <p>
-          Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
-        </p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    </>
-  );
-};
+// src/UserInfo.jsx
+import React from "react";
+import UserDetails from "./UserDetails";
 
-export default UserProfile;
+function UserInfo() {
+  return (
+    <div>
+      <h2>User Info</h2>
+      <UserDetails />
+    </div>
+  );
+}
+
+export default UserInfo;
