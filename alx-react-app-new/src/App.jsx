84662952c227d633ffb4d4941264b24267e9862b
Amer <amermmhamad@gmail.com>
2024-09-21 03:34:51 +0300

    asfaf

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +18,7 @@ function App() {
         <WelcomeMessage />
         <Header />
         <MainContent />
+        <Counter />
         <Footer />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <a href="https://vitejs.dev" target="_blank">
---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import React from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Current count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(setCount(0))}>Reset</button>
+    </div>
+  );
+}
